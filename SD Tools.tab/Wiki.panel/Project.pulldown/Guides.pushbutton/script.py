d6f4408c3b08f8d721cc758cfc35871be9d7bf7e
Simon Palmer <spalmer@shubindonaldson.com>
2020-12-15 09:00:58 -0800

    Updated links and added LA Building Code

---
+++ SD Tools.tab/Wiki.panel/Project.pulldown/Guides.pushbutton/script.py
@@ -0,0 +1,2 @@
+import webbrowser as wb
+wb.open_new(r'https://shubinanddonaldson.sharepoint.com/sites/REVIT/Shared%20Documents/General/Guides')
--- SD Tools.tab/Wiki.panel/Project.pulldown/Reference Sheet Sets.pushbutton/script.py
+++ SD Tools.tab/Wiki.panel/Project.pulldown/Reference Sheet Sets.pushbutton/script.py
@@ -1,2 +1,2 @@
 import webbrowser as wb
-wb.open_new(r'\\sdlavfs1\la_Revit_Production\REVIT STANDARDS - GOLDEN SHEETS\CURRENT')
+wb.open_new(r'https://shubinanddonaldson.sharepoint.com/sites/REVIT/Shared%20Documents/General/Reference%20Sheet%20Sets')
